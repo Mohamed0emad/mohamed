b67e8823c871d43a77dc95bcc1e9eb2dfbce3395
Mohamed Emad <153327897+Mohamed0emad@users.noreply.github.com>
2026-02-13 01:55:18 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,6 +14,54 @@ provider "aws" {
   secret_key = var.secret_key
 }
 
+#create an main vpc
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+# create Subnet
+resource "aws_subnet" "main" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "main-subnet"
+  }
+}
+
+# create Internet Gateway and attah it with VPC
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-gw"
+  }
+}
+
+# create Route Table attach with Subnet
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  tags = {
+    Name = "main-rt"
+  }
+}
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.rt.id
+}
+
 # create security group for the ec2 instance
 resource "aws_security_group" "security_group" {
   name        = "ec2-sg-group"
