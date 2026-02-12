05aab0be07661bfb814a7021009569d16e091305
Mohamed Emad <153327897+Mohamed0emad@users.noreply.github.com>
2026-02-13 01:14:56 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,14 +7,13 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
   region     = "us-east-1"
   access_key = var.access_key
   secret_key = var.secret_key
 }
 
-# استخدم الـ Security Group الموجود
+# استخدم الـ Security Group الموجود بدل ما تنشئ واحد جديد
 data "aws_security_group" "ec2_security_group" {
   filter {
     name   = "group-name"
