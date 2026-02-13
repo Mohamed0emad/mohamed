9397c3e59b5c2055ac359e07f64a7a902bf1bb32
Mohamed Emad <153327897+Mohamed0emad@users.noreply.github.com>
2026-02-13 02:29:49 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,63 +7,19 @@ terraform {
   }
 }
 
+# Configure the AWS Provider
 provider "aws" {
-  region     = "us-east-1"
+  region = "us-east-1"
   access_key = var.access_key
   secret_key = var.secret_key
 }
 
-# VPC
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "main-vpc"
-  }
-}
-
-# Subnet
-resource "aws_subnet" "main" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-2a"
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "main-subnet"
-  }
-}
-
-# Internet Gateway
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
-  tags = {
-    Name = "main-igw"
-  }
-}
-
-# Route Table
-resource "aws_route_table" "rt" {
-  vpc_id = aws_vpc.main.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-  tags = {
-    Name = "main-rt"
-  }
-}
-
-# Associate Route Table with Subnet
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.main.id
-  route_table_id = aws_route_table.rt.id
-}
-
-# Security Group
+# create security group for the ec2 instance
 resource "aws_security_group" "ec2_security_group" {
-  name        = "ec2-security-group"
+  name        = "ec2-securit-group"
   description = "allow access on ports 22"
-  vpc_id      = aws_vpc.main.id
 
+  # allow access on port 22
   ingress {
     description = "ssh access"
     from_port   = 22
@@ -84,21 +40,12 @@ resource "aws_security_group" "ec2_security_group" {
   }
 }
 
-# EC2 Instance
 resource "aws_instance" "Monitoring_server" {
-  ami           = "ami-00bb6a80f01f03502"   # تأكد إنها متوافقة مع region us-east-2
-  instance_type = "t2.medium"
-  subnet_id     = aws_subnet.main.id
-  vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
-  key_name      = var.key_name
-
-  tags = {
-    Name = var.instance_name
-  }
+ami = "ami-0030e4319cbf4dbf2"  
+instance_type = "c7i-flex.large"
+security_groups = [aws_security_group.ec2_security_group.name]
+key_name = var.key_name
+tags = {
+  Name: var.instance_name
 }
-
-# Output Public IP
-output "public_ip" {
-  description = "Public IP of the EC2 instance"
-  value       = aws_instance.Monitoring_server.public_ip
 }
