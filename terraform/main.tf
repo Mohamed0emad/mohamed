c2e8101bf76f610a537abcd341af0ebd53f8d061
Mohamed Emad <153327897+Mohamed0emad@users.noreply.github.com>
2026-02-14 18:51:14 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,42 +7,37 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
+# Provider configuration
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
   access_key = var.access_key
   secret_key = var.secret_key
 }
 
-# جلب بيانات الـ VPC الافتراضية
-data "aws_vpc" "my-vpc-nti" {
-  vpc_id      = "vpc-085567c80ce4afc2a"
-}
-resource "aws_security_group" "ec2_security_group" {
-  name        = "ec2 security group"
-  description = "allow access on ports 22"
+# Vpc ID
+data "aws_vpc" "selected" {
+  id = "vpc-085567c80ce4afc2a"
 }
 
-# create security group for the ec2 instance
+#  create Security Group and attach VPC
 resource "aws_security_group" "ec2_security_group" {
-  name        = "ec2-security-group"
-  description = "allow access on ports 22"
-  vpc_id      = data.aws_vpc.my-vpc-nti.id
+  name        = "monitoring-server-sg"
+  description = "allow access on port 22"
+  vpc_id      = data.aws_vpc.selected.id # الربط الصحيح بالـ VPC بتاعتك
 
-  # allow access on port 22
   ingress {
-    description = "ssh access"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    description      = "ssh access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = -1
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   tags = {
@@ -50,12 +45,21 @@ resource "aws_security_group" "ec2_security_group" {
   }
 }
 
+# crate EC2 Instance
 resource "aws_instance" "Monitoring_server" {
-ami = "ami-0938a60d87953e820"  
-instance_type = "c7i-flex.large"
-security_groups = [aws_security_group.ec2_security_group.name]
-key_name = var.key_name
-tags = {
-  Name: var.instance_name
+  ami           = "ami-0938a60d87953e820"
+  instance_type = "c7i-flex.large"
+  
+  # use vpc_security_group_ids  
+  vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
+  
+  key_name = var.key_name
+
+  tags = {
+    Name = var.instance_name
+  }
 }
+
+output "instance_ip" {
+  value = aws_instance.Monitoring_server.public_ip
 }
