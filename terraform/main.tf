9ba9b6d9bf4927ad86070d815ac1d0ea120f3a73
Mohamed Emad <153327897+Mohamed0emad@users.noreply.github.com>
2026-02-14 18:22:25 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,61 +9,17 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region = "ca-central-1"
   access_key = var.access_key
   secret_key = var.secret_key
 }
-# VPC
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "main-vpc"
-  }
-}
 
-# Subnet
-resource "aws_subnet" "main" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "main-subnet"
-  }
-}
-
-# Internet Gateway
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
-  tags = {
-    Name = "main-igw"
-  }
-}
-
-# Route Table
-resource "aws_route_table" "rt" {
-  vpc_id = aws_vpc.main.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-  tags = {
-    Name = "main-rt"
-  }
-}
-
-# Associate Route Table with Subnet
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.main.id
-  route_table_id = aws_route_table.rt.id
-}
-
-# Security Group
+# create security group for the ec2 instance
 resource "aws_security_group" "ec2_security_group" {
   name        = "ec2-security-group"
   description = "allow access on ports 22"
-  vpc_id      = aws_vpc.main.id
 
+  # allow access on port 22
   ingress {
     description = "ssh access"
     from_port   = 22
@@ -84,15 +40,12 @@ resource "aws_security_group" "ec2_security_group" {
   }
 }
 
-# EC2 Instance
 resource "aws_instance" "Monitoring_server" {
-  ami           = "ami-0030e4319cbf4dbf2"   # Ubuntu Jammy 22.04 في us-east-1
-  instance_type = "c7i-flex.large"
-  subnet_id     = aws_subnet.main.id
-  vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
-  key_name      = var.key_name
-
-  tags = {
-    Name = var.instance_name
-  }
+ami = "ami-0938a60d87953e820"  
+instance_type = "c7i-flex.large"
+security_groups = [aws_security_group.ec2_security_group.name]
+key_name = var.key_name
+tags = {
+  Name: var.instance_name
+}
 }
